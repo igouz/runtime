426132433c9861858e240554430774345c800f0e
igouz <499729405@qq.com>
2016-10-15 11:16:56 +0800

    Initial Commit

---
+++ runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtime
+//
+//  Created by kuanhongchai on 16/10/15.
+//  Copyright © 2016年 glhmtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runtime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  runtime
+//
+//  Created by kuanhongchai on 16/10/15.
+//  Copyright © 2016年 glhmtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
